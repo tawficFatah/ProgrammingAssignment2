bed7449b6db7577a992cc1a51c765336e230802d
Tawfic A.Fatah <tawficFatah@gmail.com>
2016-05-21 00:14:11 -0400

    coded and tested the required functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below invert a square matrix by first:
+## 1) Checking if the operation has been executed already. If so, the cached inverted matrix is returned.
+## 2) If this is the first time the matrix is being inverted, we invert the matrix, and then
+##    cache the result for future use.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Return a list of functions operational over a matrix
+makeCacheMatrix <- function(aMatrix = matrix()) {
+  matrixInverse <- NULL
+  
+  setMatrix <- function(someMatrix) {
+    matrixInverse <<- NULL
+    aMatrix <<- someMatrix
+  }
+  
+  getMatrix <- function() {
+    aMatrix
+  }
+  
+  setMatrixInverse <- function(inverse) {
+    matrixInverse <<- inverse
+  }
+  
+  getMatrixInverse <- function() {
+    matrixInverse
+  }
+  
+  list(set=setMatrix, get=getMatrix, getInverse=getMatrixInverse , setInverse=setMatrixInverse)
+  
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Invert a matrix. If the solution has been cached, return it, otherwise, invert the matrix and
+##cache the result.
+cacheSolve <- function(aMatrix, ...) {
+  ## Return a matrix that is the inverse of 'aMatrix'
+  matrixInverse <- aMatrix$getInverse()
+  
+  #Is this the first time we are inverting the matrix ?
+  if (is.null(matrixInverse)) {
+    matrixInverse <- solve(aMatrix$get())
+    aMatrix$setInverse(matrixInverse)
+    
+  }
+  
+  aMatrix$getInverse()
 }
